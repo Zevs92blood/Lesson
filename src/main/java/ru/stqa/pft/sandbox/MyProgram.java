66103c0eb776d9f8dcc0bb290e55290b66bf4b9a
Zevs <Zevs92blood@gmail.com>
2019-06-16 20:59:10 +0400

    Переменные

--- src/main/java/ru/stqa/pft/sandbox/MyProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/MyProgram.java
@@ -4,7 +4,11 @@ public class MyProgram
 {
 
     public static void main (String[] args) {
+
+        int storonaK = 6;
+        int ploschad = storonaK * storonaK;
         System.out.println("Hi");
+        System.out.println("Площадь квадрата со стороной " + storonaK + " = " + ploschad);
     }
 }
 
