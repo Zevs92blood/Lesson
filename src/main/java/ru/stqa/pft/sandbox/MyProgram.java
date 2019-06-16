a092a313d512f930fd6184f8d10a7b4ee583c17c
Zevs <Zevs92blood@gmail.com>
2019-06-16 21:24:10 +0400

    Обращение к чёртовым функциям

--- src/main/java/ru/stqa/pft/sandbox/MyProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/MyProgram.java
@@ -5,10 +5,25 @@ public class MyProgram
 
     public static void main (String[] args) {
 
-        int storonaK = 6;
-        int ploschad = storonaK * storonaK;
-        System.out.println("Hi");
-        System.out.println("Площадь квадрата со стороной " + storonaK + " = " + ploschad);
+        double storonaK = 6;
+        double a = 4;
+        double b = 6;
+
+        String slovo = "Площадь фигуры со стороной ";
+        hello("peple");
+        System.out.println(slovo + storonaK + " = " + plosch(storonaK));
+        System.out.println(slovo + a + " и " + b + " = " + plosch(a, b));
+    }
+    public static void hello(String priv){
+
+        System.out.println("Hi " + priv);
+    }
+    public static double plosch(double storonaKvadrata) {
+        return storonaKvadrata * storonaKvadrata;
+    }
+
+    public static double plosch(double storonaA, double storonaB) {
+        return storonaA * storonaB;
     }
 }
 
