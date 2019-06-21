df7f08922c94ecf0ae5136f9afb7c238a41f4b6f
Zevs <Zevs92blood@gmail.com>
2019-06-21 11:51:23 +0400

    Сменил импорт тестовых библеотек

--- src/test/java/ru/stqa/pft/sandbox/KvTest.java
+++ src/test/java/ru/stqa/pft/sandbox/KvTest.java
@@ -1,4 +1,12 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.annotations.Test;
+
 public class KvTest {
+
+    @Test
+    public void testArea(){
+        Kvadrat s = new Kvadrat(5, "e");
+           assert s.plosch() == 25;
+    }
 }
